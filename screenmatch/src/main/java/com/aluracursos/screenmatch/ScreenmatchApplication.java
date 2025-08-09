366749105fd4a2652e3ae2c1f6b84fa2b8050af8
Carlos / <153391284+CarlosRW@users.noreply.github.com>
2025-08-09 01:23:04 -0600

    integración con API, manejo de anotaciones y uso de lambdas
    - Implementado consumo de API con consultas detalladas y manipulación de datos.
    - Añadido uso de @JsonAlias y @JsonIgnoreProperties para mapear datos de la API.
    - Creado método de interacción con el usuario para búsqueda de series.
    - Implementada manipulación de cadenas para generar URLs de la API.
    - Introducción de expresiones lambda para código más eficiente.

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,28 +1,48 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.principal.Principal;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
 
-	}
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Loki&apikey=efc7f80b");
-		//var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.muestraElMenu();
+
+//        var consumoApi = new ConsumoAPI();
+//        var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Loki&apikey=efc7f80b");
+//        //var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+//        System.out.println(json);
+//        ConvierteDatos conversor = new ConvierteDatos();
+//        var datos = conversor.obtenerDatos(json, DatosSerie.class);
+//        System.out.println(datos);
+//        json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Loki&Season=1&Episode=1&apikey=efc7f80b");
+//        DatosEpisodio episodioDatos = conversor.obtenerDatos(json, DatosEpisodio.class);
+//        System.out.println(episodioDatos);
+//
+//        List<DatosTemporadas> temporadas = new ArrayList<>();
+//        for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
+//            json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=Loki&Season=" + i + "&apikey=efc7f80b");
+//            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+//            temporadas.add(datosTemporadas);
+//        }
+//        temporadas.forEach(System.out::println);
+    }
 }
